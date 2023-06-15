823637736bf495cd39a1562971185f31e51f3c94
camarguin19 <126774745+camarguin19@users.noreply.github.com>
2023-06-15 09:57:31 -0300

    Add files via upload

---
+++ reeposi/sistema/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container text-center mt-3 mb-1">
+        <img src="recursos/logo-nike-11550726013nlp37aisvx-removebg-preview.png" alt="" width="400px">
+    </div>    
+    
+    <div class="container text-center mb-5">
+        <h2>Tela de login</h2>
+    </div>
+
+    <div class="container text-center mt-5">
+        <form action="" method="post">
+            <label for="usuario">Usuário:</label>
+            <input type="email" name="" id="usuario ">
+
+            <label for="senha">Senha:</label>
+            <input type="password" name="" id="senha">
+
+            <input type="submit" value="Login">
+            <a href="cadastro.php">Cadastrar</a>
+        </form>
+
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+</html>
